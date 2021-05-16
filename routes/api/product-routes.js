da9e2ad36dd7697773e3d599a0c81eefec5f4455
grif <gmpaier@loyola.edu>
2021-05-16 19:53:46 -0400

    completely rewrote update product route to properly support updating a product which does not have associated tags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -65,7 +65,7 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  console.log(req.body);
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -91,45 +91,88 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update product data
-  Product.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((product) => {
-      // find all associated tags from ProductTag
-      return ProductTag.findAll({ where: { product_id: req.params.id } });
-    })
-    .then((productTags) => {
-      // get list of current tag_ids
-      const productTagIds = productTags.map(({ tag_id }) => tag_id);
-      // create filtered list of new tag_ids
-      const newProductTags = req.body.tagIds
-        .filter((tag_id) => !productTagIds.includes(tag_id))
-        .map((tag_id) => {
-          return {
-            product_id: req.params.id,
-            tag_id,
-          };
-        });
-      // figure out which ones to remove
-      const productTagsToRemove = productTags
-        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-        .map(({ id }) => id);
+  try {
+    const productData = await Product.update(req.body, {
+      where: {
+        id: req.params.id,
+      }});
 
-      // run both actions
-      return Promise.all([
-        ProductTag.destroy({ where: { id: productTagsToRemove } }),
-        ProductTag.bulkCreate(newProductTags),
-      ]);
-    })
-    .then((updatedProductTags) => res.json(updatedProductTags))
-    .catch((err) => {
-      // console.log(err);
-      res.status(400).json(err);
-    });
+      if (!(req.body.tagIds === undefined)) {
+        const productTagData = await ProductTag.findAll({ where: { product_id: req.params.id } });
+        const productTags = productTagData.map((productTag) => productTag.get({ plain: true}));
+        const productTagIds = productTags.map(({ tag_id }) => tag_id);
+
+        const getNewProductTags = () => {
+          if (!req.body.tagIds) {
+            return null;
+          }
+          else {
+            if (Array.isArray(req.body.tagIds)){
+              return req.body.tagIds
+              .filter((tag_id) => !productTagIds.includes(tag_id))
+              .map((tag_id) => {
+                return {
+                  product_id: req.params.id,
+                  tag_id,
+                };
+              });
+            }
+            else if (productTagIds.includes(req.body.tagIds)) {
+              return null;
+            }
+            else {
+              return [{product_id: req.params.id, tag_id: req.body.tagIds}];
+            }
+          }
+        }
+
+        const newProductTags = getNewProductTags();
+        console.log("newProductTags");
+        console.log(newProductTags);
+
+        const getProductTagsToRemove = () => {
+          if (req.body.tagIds === null){
+            return productTags;
+          }
+          else {
+            return productTags
+            .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+            .map(({ id }) => id);
+          }
+        }
+        const productTagsToRemove = getProductTagsToRemove();
+        console.log("productTagsToRemove");
+        console.log(productTagsToRemove);
+
+        if (productTagsToRemove){
+          await ProductTag.destroy({ where: { id: productTagsToRemove } });
+        }
+        if (newProductTags){
+          await ProductTag.bulkCreate(newProductTags);
+        }
+      }
+
+      const updatedProductData = await Product.findByPk(req.params.id, {
+        include: [
+          {
+            model: Category,
+            attributes: ['category_name'],
+          },
+          {
+            model: Tag, as: "tag_info",
+            attributes: ['tag_name'],
+          },
+        ]
+      })
+
+      const updatedProductInfo = updatedProductData.get({plain: true});
+      res.status(200).json(updatedProductInfo);
+  }
+  catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 
